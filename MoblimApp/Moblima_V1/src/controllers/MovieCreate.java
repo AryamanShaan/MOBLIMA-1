50d4fbaa2d82cbcb0a110f4d8bc18779c7633cc1
divyeshmundhra <46834075+divyeshmundhra@users.noreply.github.com>
2019-11-15 22:11:18 +0800

    Update MovieCreate.java

--- MoblimApp/Moblima_V1/src/controllers/MovieCreate.java
+++ MoblimApp/Moblima_V1/src/controllers/MovieCreate.java
@@ -3,9 +3,9 @@ import java.io.*;
 import models.*;
 
 public class MovieCreate {
-	public static void WriteMovie(String name, String type, String status, String rating, String synopsis, String director, String[] cast) throws IOException {
+	public static void WriteMovie(String name, String type, String status, String rating, String synopsis, String director, String[] cast, boolean x) throws IOException {
 		
-		Movie movie = new Movie(name, type, status, synopsis, director, cast, rating);
+		Movie movie = new Movie(name, type, status, synopsis, director, cast, rating, x);
 		movie.writeMovie();
 		System.out.println("Movie: "+ name + " created in the database!");
 	}
