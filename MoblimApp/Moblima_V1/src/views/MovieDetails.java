65e9a2411f0d14a8d10fe644befa4e7148c8fc21
Ritik Bhatia <42836167+ritikbhatia@users.noreply.github.com>
2019-11-15 22:24:11 +0800

    Update MovieDetails.java

--- MoblimApp/Moblima_V1/src/views/MovieDetails.java
+++ MoblimApp/Moblima_V1/src/views/MovieDetails.java
@@ -1,81 +1,68 @@
 package views;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
 
 /**
- * Obatin the ratings for a movie by a user and input them in a txt
+ * The view to get the details of all the available movies
  * @author Anon
  *
  */
-public class Review {
-	
+public class MovieDetails {
+	 
 	/**
 	 * The main function for this view
 	 * @param args for the main function
 	 * @throws Exception to throw the exception
 	 */
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception{
+		Scanner sc_input = new Scanner(System.in);
+		
 		System.out.println("-----------------------------------");
-		System.out.println("Leave a rating");
+		System.out.println("Movie Details");
 		System.out.println("-----------------------------------");
 		
-		
-		int i = 1;
-		String line = null;
-		HashMap<Integer,String> movies= new HashMap<Integer,String>();
-        FileReader fileReader = new FileReader("Movie.txt");
+		//take query by user
+		System.out.println("Enter the movie name to know its details:");
+		String movie_query = sc_input.nextLine();
+		int flag = 0;
+		String fileName = "Movie.txt";
+		String line;
+		FileReader fileReader = new FileReader(fileName);
         BufferedReader bufferedReader = new BufferedReader(fileReader); 
         while((line = bufferedReader.readLine()) != null) {
             //System.out.println(line);
             String[] x = line.split(",");
-	    movies.put(i, x[0]);
-            System.out.println(i++ +". " + x[0]);
+            if(movie_query.equals(x[0])) {
+            	flag = 1;
+            	System.out.println("The movie details are as follows:");
+				System.out.println("Movie name: "+x[0]);
+				if(x[1]=="true") {
+					System.out.println("3D or no?: Yes");
+				}
+				else {
+					System.out.println("3D or no?: No");
+				}
+				System.out.println("Genre: "+x[2]);
+				System.out.println("Release details: "+x[3]);
+				System.out.println("Age restrictions: "+x[4]);
+				System.out.println("Synopsis: " +x[5]);
+				System.out.println("Movie director: " +x[6]);
+				//Array.copyOfRange to take sub-array
+				System.out.println("Cast: "+String.join(", ",Arrays.copyOfRange(x, 7, x.length)));
+				break;
+            }
         }   
         bufferedReader.close();
-        System.out.print("Enter the movie you want to write a review for: ");
-        int movie_number = sc.nextInt();
-        String movie_to_review = movies.get(movie_number);
-        
-        System.out.println();
-        System.out.println("Enter your name:");
-        String name = sc.next();
-        
-        System.out.println();
-        System.out.println("Enter the rating for the movie:");
-        double rating = sc.nextDouble();
-        
-        String Save = movie_to_review+","+name+","+rating;
-        
-		try {
-			File file = new File("review.txt");
-			FileWriter fr = new FileWriter(file, true);
-			BufferedWriter br = new BufferedWriter(fr);
-			PrintWriter pr = new PrintWriter(br);
-			pr.println(Save);
-			pr.close();
-			br.close();
-			fr.close();
+        if(flag == 0) {
+        	System.out.println();
+			System.out.println("Movie does not exist in the database!");
 			System.out.println();
-			System.out.println("Rating entered successfully!");
-			System.out.println();
-			UserFunctions.main(null);
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+        }
+        UserFunctions.main(null);
 	}
-
+	
 }
